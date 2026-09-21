9ac84e4ca4728d019ede5457d996e3acdb533576
Gabriel <gabriel.lavoro10@gmail.com>
2026-09-21 21:21:15 +0200

    project init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+      <main className="flex min-h-screen items-center justify-center bg-slate-100">
+        <h1 className="text-4xl fontold text-violet-600">
+          Crono Frontend Challenge
+        </h1>
+      </main>
+  )
+}
+
+export default App
